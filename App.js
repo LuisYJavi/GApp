663ee30730798f49502f7fd00c597917e251293c
Javier EM <javier.emateo@gmail.com>
2018-11-26 22:15:15 +0100

    con cabecera

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import Header from './src/components/Header';
+import HeaderIconExample from './src/components/Header';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,31 +15,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       
-      <View style={styles.container}>
-      <Header />
-        <Text style={styles.welcome}>TEST</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+      <HeaderIconExample></HeaderIconExample>
+      
+        
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-const Header = (props) => {
-     
+import React, { Component } from 'react';
+import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+export default class HeaderIconExample extends Component {
+  render() {
     return (
-      <View>
-        <Text>CABEECERAAAAAAAAAAAAAAAAAA</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Right>
+        </Header>
+      </Container>
     );
-  };
-
-export default Header;
+  }
+}
