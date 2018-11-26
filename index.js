0d4322de9d7ec599af207e160f2b82a45afea19a
Javier EM <javier.emateo@gmail.com>
2018-11-26 21:44:00 +0100

    ok

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 /** @format */
 
-=======
->>>>>>> ef9d3618716c18cb06ba51961722cce94924eefb
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
