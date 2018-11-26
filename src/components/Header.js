6f128b69a69ee72a8cc94cc09d3f6b2d3ca8701b
Javier EM <javier.emateo@gmail.com>
2018-11-26 22:07:47 +0100

    ok

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+     
+    return (
+      <View>
+        <Text>CABEECERAAAAAAAAAAAAAAAAAA</Text>
+      </View>
+    );
+  };
+
+export default Header;
