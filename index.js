280aace8074406c92cc2962e5d63dc12e940cd81
Javier EM <javier.emateo@gmail.com>
2018-11-26 21:13:48 +0100

    ok

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-/** @format */
-
-=======
-<<<<<<< HEAD
-=======
-/** @format */
-
->>>>>>> 3a54587179c4334dc3153edc8951c459291a44f2
->>>>>>> dc92edd78a3d019c29a0f3cc1d65867bebdaf287
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 3a54587179c4334dc3153edc8951c459291a44f2
->>>>>>> dc92edd78a3d019c29a0f3cc1d65867bebdaf287
 AppRegistry.registerComponent(appName, () => App);
